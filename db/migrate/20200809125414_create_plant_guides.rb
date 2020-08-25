7a939218c6d218d88d6aec22d21c1e6ca48ba21f
Jacques <jacques.leroux3@gmail.com>
2020-08-25 11:44:11 +0200

    add to dbase

--- db/migrate/20200809125414_create_plant_guides.rb
+++ db/migrate/20200809125414_create_plant_guides.rb
@@ -4,8 +4,10 @@ class CreatePlantGuides < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :science_title
       t.string :image
+      t.string :season
       t.string :sun
       t.string :water
+      t.string :depth
       t.string :distance
       t.string :ph
       t.text :tips
